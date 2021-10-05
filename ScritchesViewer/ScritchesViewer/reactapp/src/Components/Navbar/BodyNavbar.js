0f6d2f5775f243d7d0356b82eb5a98caae27bf91
DeerisLeGris <35083014+DeerisLeGris@users.noreply.github.com>
2021-10-05 22:31:22 +0200

    Update BodyNavbar.js

--- ScritchesViewer/ScritchesViewer/reactapp/src/Components/Navbar/BodyNavbar.js
+++ ScritchesViewer/ScritchesViewer/reactapp/src/Components/Navbar/BodyNavbar.js
@@ -3,24 +3,60 @@ import { Flex, Skeleton, Image } from "@chakra-ui/react";
 
 import FursuitProfile from "./FursuitProfile";
 
+//Component with generated skeletons (between 1 and 11)
+const Skeletons = () => {
+    return Array.from(Array(1 + Math.floor(Math.random() * 10)).keys()).map(() => (
+        <Skeleton borderRadius="15px" height="150px"/>
+    ));
+}
+
 
 function BodyNavbar({fursuitsData, fursuitsDataLoading, onFursuitSelected, imagesData, imagesDataLoading, onImageSelected, showMode}) {
 
-    return(
+    //Loading skeletons
+    if((showMode === "images" && imagesDataLoading) || (showMode === "search" && fursuitsDataLoading))
+        return(
         <Flex flexDirection="column" overflow="auto" gridGap="10px">
-            {
-            showMode === "search" && fursuitsData && fursuitsData.fursuits ? fursuitsData.fursuits.map((fursuit) => { return(<FursuitProfile key={fursuit.id} onClick={() => onFursuitSelected(fursuit.slug, fursuit)} name={fursuit.name} image={fursuit.avatar} maker={fursuit.makers[0] != null ? fursuit.makers[0].name : ""}/>) }) : null
-
-            || showMode === "search" && fursuitsDataLoading && (<><Skeleton height="150px"/><Skeleton height="150px"/><Skeleton height="150px"/></>)
-            }
-
-            {
-                showMode === "images" && imagesData && imagesData.fursuitMedia ? imagesData.fursuitMedia.map((image) => { return(<Image key={image.id} src={image.thumbnail} onClick={() => onImageSelected(image.id)} cursor="pointer"/>) }) : null
-
-                || showMode === "images" && imagesDataLoading && (<><Skeleton height="150px"/><Skeleton height="150px"/><Skeleton height="150px"/></>)
-            }
+            <Skeletons/>
         </Flex>
-    )
+        )
+
+    //Show results (searchs results OR images results)
+    switch(showMode) {
+        case "search":
+            return(
+                <Flex flexDirection="column" overflow="auto" gridGap="10px">
+                    {
+                        fursuitsData && fursuitsData.fursuits && fursuitsData.fursuits.map((fursuit) => (
+                            <FursuitProfile key={fursuit.id} 
+                                            onClick={() => onFursuitSelected(fursuit.slug, fursuit)} 
+                                            name={fursuit.name} 
+                                            image={fursuit.avatar} 
+                                            maker={fursuit.makers[0] != null ? fursuit.makers[0].name : ""}
+                            />
+                        ))
+                    }
+                </Flex>
+            );
+            break;
+
+        case "images":
+            return(
+                <Flex flexDirection="column" overflow="auto" gridGap="10px">
+                    {
+                        imagesData && imagesData.fursuitMedia && imagesData.fursuitMedia.map((image) => (
+                            <Image  key={image.id} 
+                                    src={image.thumbnail} 
+                                    onClick={() => onImageSelected(image.id)} 
+                                    cursor="pointer"
+                            />
+                        ))
+                    }
+                </Flex>
+            );
+            break;
+    }
+
 }
 
 export default BodyNavbar;
