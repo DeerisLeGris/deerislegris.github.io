53e28a97b0d46ba37a278a8f19fe6c8ca0d6793b
DeerisLeGris <35083014+DeerisLeGris@users.noreply.github.com>
2021-10-05 22:43:28 +0200

    Update TopNavbar.js

--- ScritchesViewer/ScritchesViewer/reactapp/src/Components/Navbar/TopNavbar.js
+++ ScritchesViewer/ScritchesViewer/reactapp/src/Components/Navbar/TopNavbar.js
@@ -1,36 +1,64 @@
 //Libraries
 import { Flex, Input, InputGroup, InputRightElement, Button, Image, CloseButton, Text } from "@chakra-ui/react";
 import { SearchIcon } from '@chakra-ui/icons';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useCallback } from 'react';
 
 function TopNavbar({onSearch, onCancel, showMode, selectedFursuit}) {
     const [searchValue, setSearchValue] = useState("");
-    const onInput = (event) => { 
+
+    const handleKeyDown = useCallback((event) => { 
         if(event.key === "Enter")
             onSearch(searchValue);
-    };
+    }, [searchValue, onSearch]);
+
+    const handleChange = useCallback((event) => setSearchValue(event.target.value), []);
+
+    const handleSearchButton = useCallback(() => onSearch(searchValue), [searchValue, onSearch]);
+
+    const handleCancelButton = useCallback(() => onCancel(), [onCancel]);
 
     return(
-        <Flex flexShrink={0} flexDirection="column" alignItems="stretch" justifyContent="center">
-            {showMode === "search" && (
-            <InputGroup>
-                <Input placeholder="Search fursuit..." value={searchValue} onChange={event => setSearchValue(event.target.value)} onKeyDown={onInput}/>
-                <InputRightElement width="4rem">
-                    <Button h="1.75rem" size="sm" onClick={() => onSearch(searchValue)}><SearchIcon color="gray.300"/></Button>
-                </InputRightElement>
-            </InputGroup>
-            )}
+        <Flex flexShrink={0} 
+              flexDirection="column" 
+              alignItems="stretch" 
+              justifyContent="center">
+            {
+                showMode === "search" && (
+                    <InputGroup>
+
+                        <Input placeholder="Search fursuit..." 
+                               value={searchValue} 
+                               onChange={handleChange} 
+                               onKeyDown={handleKeyDown}/>
+
+                        <InputRightElement width="4rem">
+                            <Button h="1.75rem" size="sm" onClick={handleSearchButton}>
+                                <SearchIcon color="gray.300"/>
+                            </Button>
+                        </InputRightElement>
+
+                    </InputGroup>
+                )
+            }
 
             {
                 showMode === "images" && (
-                <Flex flexGrow={0} flexShrink={0} flexDirection="row" alignItems="center" borderRadius="15px">
-                    <Image src={selectedFursuit.avatar} height="50" borderRadius="15px"/>
-                    <Flex marginLeft="10px" flexDirection="column" flexGrow={1}>
-                        <Text fontWeight="bold">{selectedFursuit.name}</Text>
-                        <Text fontStyle="italic">{selectedFursuit.makers[0] != null ? selectedFursuit.makers[0].name : ""}</Text>
+                    <Flex flexGrow={0} 
+                        flexShrink={0} 
+                        flexDirection="row" 
+                        alignItems="center" 
+                        borderRadius="15px">
+
+                        <Image src={selectedFursuit.avatar} height="50" borderRadius="15px"/>
+
+                        <Flex marginLeft="10px" flexDirection="column" flexGrow={1}>
+                            <Text fontWeight="bold">{selectedFursuit.name}</Text>
+                            <Text fontStyle="italic">{selectedFursuit.makers[0] != null ? selectedFursuit.makers[0].name : ""}</Text>
+                        </Flex>
+
+                        <CloseButton onClick={handleCancelButton}/>
+
                     </Flex>
-                    <CloseButton onClick={() => onCancel()}/>
-                </Flex>
                 )
             }
         </Flex>
