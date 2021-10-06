8f7a948c82f2dc078c792e902e16681dc0edcde0
DeerisLeGris <35083014+DeerisLeGris@users.noreply.github.com>
2021-10-06 19:04:01 +0200

    Update Body.js

--- ScritchesViewer/ScritchesViewer/reactapp/src/Components/Body.js
+++ ScritchesViewer/ScritchesViewer/reactapp/src/Components/Body.js
@@ -1,5 +1,5 @@
 import { Flex, Text } from "@chakra-ui/react";
-import { useLazyQuery, gql } from "@apollo/client";
+import { useQuery, useLazyQuery, gql } from "@apollo/client";
 import React, { useEffect } from 'react';
 import { useParams } from "react-router-dom";
 
@@ -13,20 +13,26 @@ const GET_IMAGE = gql`query Medium($id: ID!, $tagging: Boolean) {
 
 function Body() {
     const {idimage} = useParams();
-    const [getImage, { loading, error, data }] = useLazyQuery(GET_IMAGE);
-
-    useEffect(() => {
-        if(idimage) {
-            getImage({ variables: { id: idimage } });
-        }
-    }, [idimage]);
+    const { loading, error, data } = useQuery(GET_IMAGE, { 
+        variables: { id: idimage }, 
+        skip: !idimage 
+    });
 
     if(data && data.medium) {
-        return(<Flex flexGrow={1} backgroundImage={"url('" + data.medium.picture + "')"} backgroundSize="contain" backgroundPosition="center" backgroundRepeat="no-repeat" backgroundColor="gray.600"/>);
+        return(<Flex flexGrow={1} 
+                     backgroundImage={"url('" + data.medium.picture + "')"} 
+                     backgroundSize="contain" backgroundPosition="center" 
+                     backgroundRepeat="no-repeat" 
+                     backgroundColor="gray.600"/>);
     }
 
-    return(<Flex flexGrow={1} backgroundColor="gray.600" alignItems="center" justifyContent="center">
+    return(<Flex flexGrow={1} 
+                 backgroundColor="gray.600" 
+                 alignItems="center" 
+                 justifyContent="center">
+
         <Text color="whitesmoke" fontSize="5xl">Scritch.es viewer</Text>
+
     </Flex>);
 
 }
